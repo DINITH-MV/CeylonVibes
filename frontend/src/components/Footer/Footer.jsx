bdf8c0bf0504353e4fbc73b45a4eb12de56a4026
DINITH-MV <132985087+DINITH-MV@users.noreply.github.com>
2024-02-08 09:41:58 +0530

    Footer creation

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,19 +3,19 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <div className="bg-[#000] pt-[20px] pl-[40px] font-Spirax text-[30pt] text-[#fff]">CeylonVibes
-                <i class="fa-sharp fa-thin fa-copyright text-[6px] ml-[4px]" style={{ color: '#ffffff' }}></i>
-                <div className='h-[300px] ml-[-39px] bg-[#fff] grid grid-cols-2 border border-[#fff]'>
-                    <div className='border-white grid grid-cols-2 w-[60%]'>
-                        <div className='bg-[#fff] border-white pl-[40px] text-[#000] font-Barlow text-[16px]'>
-                            <button className='mt-[10px] font-bold' href="#" >Organic Shop</button>
-                        </div>
-                        <div className="bg-[#c97b7b]"></div>
+            <div className='h-[300px] bg-[#fff] grid grid-cols-3 grid-flow-col border border-[#fff]'>
+                <div className="bg-[#000] text-center row-span-3 text-[#fff]">
+                    <div className="border border-[#fff] pt-[100px] font-Spirax text-[30pt]">CeylonVibes
+                        <i class="fa-sharp fa-thin fa-copyright text-[6px] ml-[4px] border-white" style={{ color: '#ffffff' }}></i>
                     </div>
-                    <div className="bg-[#834141] w-[60%]"></div>
+                    <div className='font-Abel'>To Enrich your Life</div>
+                    <div className="pd"></div>
                 </div>
+                <div className="bg-[#834141] col-span-2"></div>
+                <div className="bg-[#d4b7b7] row-span-2 "></div>
             </div>
         </div>
+
     );
 }
 
