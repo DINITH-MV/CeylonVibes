a4fe34721f6f9ca041bc392a3cc743ded00b6fee
DINITH-MV <132985087+DINITH-MV@users.noreply.github.com>
2024-03-10 17:22:51 +0530

    Deleted unwanted things

--- backend/models/storeModel.js
+++ backend/models/storeModel.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-
 const catagorySchema = new Schema(
   {
     name: {
@@ -38,50 +37,6 @@ const productsSchema = new Schema({
 });
 
 export const Products = mongoose.model("products", productsSchema);
-
-const SnacksSchema = new Schema({
-  
-  name: {
-    type: String,
-  },
-  price: {
-    type: String,
-  },
-  discPrice: {
-    type: String,
-  },
-  image: {
-    type: String,
-  },
-  imageFolder: {
-    type: String,
-  },
-});
-
-export const Snacks = mongoose.model("Snacks", SnacksSchema);
-
-const SweetnersSchema = new Schema({
-  _id: {
-    type: String,
-  },
-  name: {
-    type: String,
-  },
-  price: {
-    type: String,
-  },
-  discPrice: {
-    type: String,
-  },
-  image: {
-    type: String,
-  },
-  imageFolder: {
-    type: String,
-  },
-});
-
-export const Sweetners = mongoose.model("Sweetners", SweetnersSchema);
 
 const OffersSchema = new Schema({
   _id: {
